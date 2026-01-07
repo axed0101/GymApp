b05f505397f948e596bae0f67c9241a02b45f991
axed0101 <alessioderosas01@gmail.com>
2026-01-08 00:01:04 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,38 @@
-self.addEventListener("install",e=>self.skipWaiting());
-self.addEventListener("activate",e=>self.clients.claim());
+// Simple offline cache for PWA
+const CACHE = "gymapp-offline-v13";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./data.json",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(()=>self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k===CACHE ? null : caches.delete(k)))).then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then((cached) => cached || fetch(req).then((res)=>{
+      // cache same-origin GET
+      try{
+        const url = new URL(req.url);
+        if(req.method==="GET" && url.origin===self.location.origin){
+          const copy = res.clone();
+          caches.open(CACHE).then(cache=>cache.put(req, copy));
+        }
+      }catch(e){}
+      return res;
+    }).catch(()=>caches.match("./index.html")))
+  );
+});
